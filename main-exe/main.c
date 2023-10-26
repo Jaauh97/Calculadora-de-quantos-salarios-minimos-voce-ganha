d94e06e26f640eeabe7e0c57f9c71fc309c166c2
Joao Pedro <jaauhp@gmail.com>
2023-10-26 15:20:21 -0300

    p

---
+++ main-exe/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+float sal , sal2, qtd_sal_min, qtd_sal_min2;
+int m; 
+const float sal_min = 1.320;
+
+printf("Digite Seu Salario Atual:\n");
+scanf("%f", &sal);
+
+printf("O Valor digitado foi de: \n");
+printf("%.3f.\n", sal);
+
+printf("O valor acima esta certo?\n");
+printf("Digite 1 para SIM e 2 para NÃO \n");
+scanf("%d", &m);
+
+if (m == 2)
+{
+  printf("Digite o Valor Correto do Seu Salario\n"); 
+ 
+}
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
